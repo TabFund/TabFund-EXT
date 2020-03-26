22833782e0072554914bf1ba609a4857ccddd419
henriquelcardoso <henrique.lcardoso@gmail.com>
2020-03-26 17:17:48 +0000

    Project clean Up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import firebase from './firebase';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-import Router from 'route-lite';
-import { useSelector, useDispatch } from 'react-redux';
-import * as actions from './store/actions/actionTypes';
-import { checkAdBlocks } from './store/actions/adsAction';
-import { goTo } from 'route-lite';
-import LoginForm from './components/authentication/LoginForm';
-import Home from './components/Home';
-import store from './store/storeIndex';
-
 const App = () => {
-  console.log("App");
-
-  const dispatch = useDispatch();
-  // const [token, setToken] =  useState(false)
-
-  useEffect(() => {
-    // dispatch(checkAdBlocks())
-    //window.location = "http://localhost:3000/"
-    // const userToken = localStorage.getItem('userToken');
-    // if (userToken) {
-    //   store.dispatch({type: actions.AUTH_SUCCESS}) 
-
-    // } else{
-    //   store.dispatch({type: actions.AUTH_LOGOUT});
-
-    // } 
-    // console.log("userToken : " + userToken);
-  })
-
-
-  const logged = useSelector(state => state.auth.logged);
-  console.log("logged : " + logged);
-
-
 
   return (
     <iframe title="w3school" src="http://localhost:3000/">
--- src/index.js
+++ src/index.js
@@ -2,35 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import firebase from './firebase';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
-import store from './store/storeIndex';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'bootstrap/dist/css/bootstrap.css'; // If Bootstrap is not working, import this file!
 
-const rrfConfig = {
-    userProfile: 'users',
-    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
-
-const rrfProps = {
-    firebase,
-    config: rrfConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance // <- needed if using firestore
-}
 
 const root = document.getElementById('root')
 
-ReactDOM.render(<Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
+ReactDOM.render(
         <App />
-    </ReactReduxFirebaseProvider>
-</Provider>
 , root);
 
 // If you want your app to work offline and load faster, you can change
