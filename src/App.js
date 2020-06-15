b94a56171680f8d3b449d8a788b56d202eedeacf
Leonardo Machado <leonardo.ccmachado@gmail.com>
2020-06-15 11:55:55 +0100

    extension update to fit all screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import $ from "jquery"
 const App = () => {
+
+  useEffect(() => {
+    resize();
+    window.addEventListener('resize', resize)
+  })
 
   const yesterdayDate = localStorage.getItem('yesterdayDate');
     if (yesterdayDate) {
@@ -25,12 +31,39 @@ const App = () => {
         localStorage.setItem('imageNumber', 1)
     }
 
+const resize = ()=>{
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  var outer = document.getElementById("outer");
+  var wrap = document.getElementById("wrap");
+  var maxWidth = outer.clientWidth;
+  var maxHeight = outer.clientHeight;
+
+  var scale=1;
+
+  scale = Math.min(width/maxWidth, height/maxHeight);  
+
+  wrap.style.width=maxWidth*scale + "px";
+  wrap.style.height=maxHeight*scale + "px";
+
+  outer.style.transform="scale(" + scale + ")";
+
+  var topPadding = (height - wrap.clientHeight)/2;
+  wrap.style.paddingTop=topPadding + "px";
+}
+
   return (
     <div>
-      <img className="backgroundImage " src={require('./img/background/joao_lopes_' + localStorage.getItem('imageNumber') + '.JPG')} alt="background" />
+    <img className="backgroundImage " src={require('./img/background/joao_lopes_' + localStorage.getItem('imageNumber') + '.JPG')} alt="background" />
+    <div id="wrap">
+      <div id="outer">
       <iframe title="TabFund" src="http://localhost:3000//">
         <p>Your browser does not support iframes.</p>
       </iframe>
+      </div>
+      
+    </div>
     </div>
     // <div className="App">
     //   <header className="App-header">
@@ -41,5 +74,27 @@ const App = () => {
     // </div>
   );
 }
+
+var maxWidth  = $('#outer').width();
+var maxHeight = $('#outer').height();
+
+$(window).resize(function(evt) {
+    var $window = $(window);
+    var width = $window.width();
+    var height = $window.height();
+    var scale;
+
+    // early exit
+    if(width >= maxWidth && height >= maxHeight) {
+        $('#outer').css({'-webkit-transform': ''});
+        $('#wrap').css({ width: '', height: '' });
+        return;
+    }
+
+    scale = Math.min(width/maxWidth, height/maxHeight);
+
+    $('#outer').css({'-webkit-transform': 'scale(' + scale + ')'});
+    $('#wrap').css({ width: maxWidth * scale, height: maxHeight * scale });
+});
 
 export default App;
